915ba4d0dd0280b63b1596ac9807fc91bca0f5fe
HamzaF69 <hamzafarhan69420@gmail.com>
2026-03-18 15:03:29 +0500

    Updated LCP

--- Easy/LongestCommonPrefix.cpp
+++ Easy/LongestCommonPrefix.cpp
@@ -3,24 +3,19 @@
 #include <string>
 using namespace std;
 
-class Solution {
+class Solution{
 public:
-    string longestCommonPrefix(vector<string>& strs) {
-        int n=strs.size();
-        int j=0;
-        bool same=true;
-        string pref="";
-        while(same==true){
-            for(int i=0;i<n-1;i++){
-                if (j >= strs[i].size() || j >= strs[i+1].size()) {
-                    return pref;
-                }
-                if(strs[i][j]!=strs[i+1][j]){
+    string longestCommonPrefix(vector<string> &strs){
+        int n = strs.size();
+        string pref = "";
+        for (int j = 0; j < strs[0].size(); j++){
+            char c=strs[0][j];
+            for (int i=1;i<n;i++){
+                if (j >= strs[i].size() || strs[i][j] != c){
                     return pref;
                 }
             }
-            pref=pref+strs[0][j];
-            j++;
+            pref+=c;
         }
         return pref;
     }
